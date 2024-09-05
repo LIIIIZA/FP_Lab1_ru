75ad398309bd6823de321647b1fb5d2aee21efa4
LIIIIZA <119616760+LIIIIZA@users.noreply.github.com>
2024-09-06 00:12:10 +0300

    Update equations.fsx

--- equations.fsx
+++ equations.fsx
@@ -31,8 +31,8 @@ let phi2 = x - f2(x) / f2'(x)
 let phi3 = x - f3(x) / f3'(x)
 
 let main = 
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 0. 1.) (iterations phi1 0.) (newthon f1 f1' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 0. 1.) (iterations phi2 0.) (newthon f2 f2' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 0. 1.) (iterations phi3 0.) (newthon f3 f3' 1.)
+    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 2. 3.) (iterations phi1 2.) (newthon f1 f1' 3.)
+    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 0.2 1.) (iterations phi2 0.2) (newthon f2 f2' 1.)
+    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 1. 2.) (iterations phi3 1.) (newthon f3 f3' 2.)
 
  
