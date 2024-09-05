4daecc5b1264ac28fead68bc4729f0924bf489fc
LIIIIZA <119616760+LIIIIZA@users.noreply.github.com>
2024-09-06 00:08:20 +0300

    Update equations.fsx

--- equations.fsx
+++ equations.fsx
@@ -1,24 +1,34 @@
 // Определите функции для решение алгебраических уравнений
 
-let dichotomy f a b = 0.
+let eps = 0.00001 
 
-let iterations phi x0 = 0.
+let rec dichotomy f a b =
+    let c = (a+b)/2.
+    if (f c) < 0.00001 then c
+    else if (f c)*(f a) < 0 then dichotomy f a c
+    else dichotomy f c b
 
-let newthon f f' x0 = 0.
-// используйте функцию 'iterations'
+let rec iterations phi x0 =
+    if abs(x0 - phi(x0)) < eps then
+        phi(x0)
+    else
+        iterations phi (phi(x0))
+
+let newthon f f' x0 = 
+    iterations (fun x->x - f(x)/f'(x)) x0
 
 // Решите 3 уравнения (начиная со своего номера варианта) с использованием 3-х методов
-let f1 = ...
-let f2 = ...
-let f3 = ...
+let f1 = log(x) - x + 1.8
+let f2 = x*tan(x) - 1./3.
+let f3 = tan(x/2.) - 1./tan(x/2.) + x
 
-let f1' = ...
-let f2' = ...
-let f3' = ...
+let f1' = 1./x -1.
+let f2' = tan(x) + x /(cos(x)**2.)
+let f3' = 0.5*(1./(cos(x/2.)**2.)+1./(sin(x/2.)**2.)) + 1.
 
-let phi1 = ...
-let phi2 = ...
-let phi3 = ...
+let phi1 = x - f1(x) / f1'(x)
+let phi2 = x - f2(x) / f2'(x)
+let phi3 = x - f3(x) / f3'(x)
 
 let main = 
     printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 0. 1.) (iterations phi1 0.) (newthon f1 f1' 1.)
